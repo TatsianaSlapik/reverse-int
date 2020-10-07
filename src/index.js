584a5cbd79ccfee5e394faea6181b8ce2df1361e
TatsianaSlapik <slapik.tatiana@gmail.com>
2020-10-07 21:45:50 +0300

    feat: updated function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString();
+    let i = str.length - 1;
+
+    let result = '';
+    while (i >= 0) {
+
+        result = `${result}${str[i]}`;
+
+        i = i - 1;
+    }
+
+    return new Number(result);
 }
